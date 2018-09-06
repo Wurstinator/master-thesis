24032362dfaabb3dd20985d2fe7296f96d515a7b
Andreas Tollkötter <tollkoetter.andreas@gmail.com>
2018-09-06 09:04:42 +0200

    Added data for path refinement

---
+++ code/src/scripts/path_refinement_statistics.py
@@ -0,0 +1,38 @@
+
+
+import argparse
+import os
+import sys
+from run_statistics import run_experiments, run_process_for_time
+
+executable = '../../bin/iterated_moore_statistics'
+
+
+class ExecIM:
+    def __init__(self, timeout):
+        self.timeout = timeout
+
+    def __call__(self, filename):
+        cmd = '{} --output=json --automaton={}'.format(executable, filename)
+        return run_process_for_time(cmd, timeout=self.timeout)
+
+
+def main():
+    if not (os.path.isfile(executable) and os.access(executable, os.X_OK)):
+        print('Error. Compile iterated_moore_statistics first.')
+        sys.exit(1)
+
+    args = parse_args()
+    run_experiments(args, ExecIM(args.timeout))
+
+
+# Parses the command line arguments.
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='One or multiple file paths to HOA files.', nargs='+')
+    parser.add_argument('-t', dest='timeout', help='Time limit in seconds for each analysis.', type=int)
+    parser.add_argument('-o', dest='output', help='Output file. If none is specified, stdout is used.')
+    return parser.parse_args()
+
+
+main()
---
+++ data/analyze_path_refinement.py
@@ -0,0 +1,4 @@
+
+from analyze_common import *
+
+general_analysis_v1(input_prefix='path_refinement/', output_prefix='path_refinement/')
