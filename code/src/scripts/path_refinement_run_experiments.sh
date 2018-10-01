dc5de3071feae45d0a8fd342cb1a7a7ad8f3368a
Andreas Tollkötter <tollkoetter.andreas@gmail.com>
2018-10-01 16:50:26 +0200

    Updated PR v2 data

--- code/src/scripts/path_refinement_run_experiments.sh
+++ code/src/scripts/path_refinement_run_experiments.sh
@@ -7,13 +7,13 @@ then
     (set -x;
     mkdir -p ../../../data/raw
     mkdir -p ../../../data/raw/path_refinement/
-    python path_refinement_statistics.py -t 90 -o ../../../data/raw/path_refinement/gendet_ap1.json "../../../data/automata/gendet/ap1/*/*"
-    python path_refinement_statistics.py -t 90 -o ../../../data/raw/path_refinement/gendet_ap2.json "../../../data/automata/gendet/ap2/*/*"
-    python path_refinement_statistics.py -t 90 -o ../../../data/raw/path_refinement/gendet_ap3.json "../../../data/automata/gendet/ap3/*/*"
-    python path_refinement_statistics.py -t 90 -o ../../../data/raw/path_refinement/detnbaut_ap1.json "../../../data/automata/detnbaut/ap1/*/*"
-    python path_refinement_statistics.py -t 90 -o ../../../data/raw/path_refinement/detnbaut_ap2.json "../../../data/automata/detnbaut/ap2/*/*"
-    python path_refinement_statistics.py -t 90 -o ../../../data/raw/path_refinement/detspot_ap1.json "../../../data/automata/detspot/ap1/*/*"
-    python path_refinement_statistics.py -t 90 -o ../../../data/raw/path_refinement/detspot_ap2.json "../../../data/automata/detspot/ap2/*/*"
+    python path_refinement_statistics.py -t 150 -o ../../../data/raw/path_refinement/gendet_ap1.json "../../../data/automata/gendet/ap1/*/*"
+    python path_refinement_statistics.py -t 150 -o ../../../data/raw/path_refinement/gendet_ap2.json "../../../data/automata/gendet/ap2/*/*"
+    python path_refinement_statistics.py -t 150 -o ../../../data/raw/path_refinement/gendet_ap3.json "../../../data/automata/gendet/ap3/*/*"
+    python path_refinement_statistics.py -t 150 -o ../../../data/raw/path_refinement/detnbaut_ap1.json "../../../data/automata/detnbaut/ap1/*/*"
+    python path_refinement_statistics.py -t 150 -o ../../../data/raw/path_refinement/detnbaut_ap2.json "../../../data/automata/detnbaut/ap2/*/*"
+    python path_refinement_statistics.py -t 150 -o ../../../data/raw/path_refinement/detspot_ap1.json "../../../data/automata/detspot/ap1/*/*"
+    python path_refinement_statistics.py -t 150 -o ../../../data/raw/path_refinement/detspot_ap2.json "../../../data/automata/detspot/ap2/*/*"
     )
 
 fi
