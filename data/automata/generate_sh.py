04bbc3a42d04239dc2825892622178caaca80a71
Andreas Tollkötter <tollkoetter.andreas@gmail.com>
2018-12-15 12:12:58 +0100

    Streamlined the code of automata generation script

--- data/automata/generate_sh.py
+++ data/automata/generate_sh.py
@@ -23,62 +23,24 @@ def generate_set(method, ap, qmin, qmax, n):
     subprocess.run(cmd, shell=True)
 
 
-generate_set('gendet', 1,   1,  10, 50)
-generate_set('gendet', 1,  11,  20, 50)
-generate_set('gendet', 1,  21,  40, 40)
-generate_set('gendet', 1,  41,  60, 30)
-generate_set('gendet', 1,  61,  90, 20)
-generate_set('gendet', 1,  91, 120, 15)
-generate_set('gendet', 1, 121, 160, 10)
-generate_set('gendet', 1, 161, 200,  5)
 
-generate_set('gendet', 2,   1,  10, 50)
-generate_set('gendet', 2,  11,  20, 50)
-generate_set('gendet', 2,  21,  40, 40)
-generate_set('gendet', 2,  41,  60, 30)
-generate_set('gendet', 2,  61,  90, 20)
-generate_set('gendet', 2,  91, 120, 15)
-generate_set('gendet', 2, 121, 160, 10)
+state_sets = [ #(qmin, qmax, n)
+    (1, 10, 50),
+    (11, 20, 50),
+    (21, 40, 40),
+    (41, 60, 30),
+    (61, 90, 20),
+    (91, 120, 15),
+    (121, 160, 10)
+]
 
-generate_set('gendet', 3,   1,  10, 50)
-generate_set('gendet', 3,  11,  20, 50)
-generate_set('gendet', 3,  21,  40, 40)
-generate_set('gendet', 3,  41,  60, 30)
-generate_set('gendet', 3,  61,  90, 20)
-generate_set('gendet', 3,  91, 120, 15)
-generate_set('gendet', 3, 121, 160, 10)
+for method in ['gendet', 'detspot', 'detnbaut']:
+    aps = [1, 2] if method != 'gendet' else [1, 2, 3]
+    for ap in aps:
+        for ssn in state_sets:
+            generate_set(method, ap, *ssn)
 
 
-generate_set('detspot', 1,   1,  10, 50)
-generate_set('detspot', 1,  11,  20, 50)
-generate_set('detspot', 1,  21,  40, 40)
-generate_set('detspot', 1,  41,  60, 30)
-generate_set('detspot', 1,  61,  90, 20)
-generate_set('detspot', 1,  91, 120, 15)
-generate_set('detspot', 1, 121, 160, 10)
-
-generate_set('detspot', 2,   1,  10, 50)
-generate_set('detspot', 2,  11,  20, 40)
-generate_set('detspot', 2,  21,  40, 30)
-generate_set('detspot', 2,  41,  60, 20)
-generate_set('detspot', 2,  61,  90, 10)
-generate_set('detspot', 2,  91, 120,  5)
-
-
-generate_set('detnbaut', 1,   1,  10, 100)
-generate_set('detnbaut', 1,  11,  20,  50)
-generate_set('detnbaut', 1,  21,  40,  40)
-generate_set('detnbaut', 1,  41,  60,  30)
-generate_set('detnbaut', 1,  61,  90,  20)
-generate_set('detnbaut', 1,  91, 120,  15)
-generate_set('detnbaut', 1, 121, 160,  10)
 generate_set('detnbaut', 1, 161, 200,  10)
 generate_set('detnbaut', 1, 201, 300,   5)
 generate_set('detnbaut', 1, 301, 400,   5)
-
-generate_set('detnbaut', 2,   1,  10, 50)
-generate_set('detnbaut', 2,  11,  20, 40)
-generate_set('detnbaut', 2,  21,  40, 30)
-generate_set('detnbaut', 2,  41,  60, 20)
-generate_set('detnbaut', 2,  61,  90, 10)
-generate_set('detnbaut', 2,  91, 120,  5)
